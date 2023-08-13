2c3edf221aec190c7ed58e72e9a955105ae61af9
JasonTollison <jason_tollison@hotmail.com>
2023-08-13 15:33:32 +1200

    Website publish fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/portfolioweb/public',
 }
 
-module.exports = {
-    basePath: '/portfolioweb',
-}
+module.exports = nextConfig
