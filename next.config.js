dbaeca87063f9785ece38da923fc25c9942a75a5
JasonTollison <jason_tollison@hotmail.com>
2026-04-05 06:19:22 +1200

    Updated the way config sets resource src prefix

--- next.config.js
+++ next.config.js
@@ -1,17 +1,37 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
-const nextConfig = {
-    output: isProd ? 'export' : 'standalone',
-    images: {
-        unoptimized: true,
-    },
-    basePath: isProd ? '/portfolioweb' : '',
-    assetPrefix: isProd ? '/portfolioweb' : '',
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
+// const nextConfig = {
+//     output: isProd ? 'export' : 'standalone',
+//     images: {
+//         unoptimized: true,
+//     },
+//     basePath: isProd ? '/portfolioweb' : '',
+//     assetPrefix: isProd ? '/portfolioweb' : '',
+//     eslint: {
+//         ignoreDuringBuilds: true,
+//     },
+// }
+
+// module.exports = nextConfig
+
+const { PHASE_PRODUCTION_BUILD } = require('next/constants')
+
+module.exports = (phase) => {
+    // Check if the current phase is the production build
+    const isProd = phase === PHASE_PRODUCTION_BUILD
+
+    return {
+        output: isProd ? 'export' : 'standalone',
+        images: {
+            unoptimized: true,
+        },
+        basePath: isProd ? '/portfolioweb' : '',
+        assetPrefix: isProd ? '/portfolioweb' : '',
+        eslint: {
+            ignoreDuringBuilds: true,
+        },
+    }
 }
 
-module.exports = nextConfig
