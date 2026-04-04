1dd402658a4d2b77e1468f25c3b6843fb85adc27
JasonTollison <jason_tollison@hotmail.com>
2026-04-05 11:41:31 +1200

    reverted config file back to old style for github actions to work correctly

--- next.config.js
+++ next.config.js
@@ -1,29 +1,58 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production'
 
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
-
-module.exports = (phase, { defaultConfig }) => {
-    // Check if the current phase is the development build
-    if (phase === PHASE_DEVELOPMENT_SERVER) {
-        return {
-            output: 'standalone',
-            images: {
-                unoptimized: true,
-            },
-        }
-    }
-
-    // Config options for all phases except development
-    return {
-        output: 'export',
-        images: {
-            unoptimized: true,
-        },
-        basePath: '/portfolioweb',
-        assetPrefix: '/portfolioweb',
-        eslint: {
-            ignoreDuringBuilds: true,
-        },
-    }
+// Prod mode
+const nextConfig = {
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+    basePath: '/portfolioweb',
+    assetPrefix: '/portfolioweb',
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 }
+// // Dev mode
+// const nextConfig = {
+//     output: 'standalone',
+//     images: {
+//         unoptimized: true,
+//     },
+//     eslint: {
+//         ignoreDuringBuilds: true,
+//     },
+// }
+
+module.exports = nextConfig
+
+
+// const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
+
+// // Goback to old config file format as github doesn't seem to like this one
+
+// module.exports = (phase, { defaultConfig }) => {
+//     // Check if the current phase is the development build
+//     if (phase === PHASE_DEVELOPMENT_SERVER) {
+//         return {
+//             output: 'standalone',
+//             images: {
+//                 unoptimized: true,
+//             },
+//         }
+//     }
+
+//     // Config options for all phases except development
+//     return {
+//         output: 'export',
+//         images: {
+//             unoptimized: true,
+//         },
+//         basePath: '/portfolioweb',
+//         assetPrefix: '/portfolioweb',
+//         eslint: {
+//             ignoreDuringBuilds: true,
+//         },
+//     }
+// }
