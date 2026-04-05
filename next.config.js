d48f4c3dfef6443066fe0142695e516330b3444c
JasonTollison <jason_tollison@hotmail.com>
2026-04-05 18:16:57 +1200

    updated nextjs to fix the prod build css issues

--- next.config.js
+++ next.config.js
@@ -1,58 +1,23 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
-
 // Prod mode
-const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
-    basePath: '/portfolioweb',
-    assetPrefix: '/portfolioweb',
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
-}
-// // Dev mode
 // const nextConfig = {
-//     output: 'standalone',
+//     output: 'export',
 //     images: {
 //         unoptimized: true,
 //     },
+//     basePath: '/portfolioweb',
+//     assetPrefix: '/portfolioweb',
 //     eslint: {
 //         ignoreDuringBuilds: true,
 //     },
 // }
+// Dev mode
+const nextConfig = {
+    allowedDevOrigins: ['192.168.1.88'],
+    images: {
+        unoptimized: true,
+    }
+}
 
 module.exports = nextConfig
-
-
-// const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
-
-// // Goback to old config file format as github doesn't seem to like this one
-
-// module.exports = (phase, { defaultConfig }) => {
-//     // Check if the current phase is the development build
-//     if (phase === PHASE_DEVELOPMENT_SERVER) {
-//         return {
-//             output: 'standalone',
-//             images: {
-//                 unoptimized: true,
-//             },
-//         }
-//     }
-
-//     // Config options for all phases except development
-//     return {
-//         output: 'export',
-//         images: {
-//             unoptimized: true,
-//         },
-//         basePath: '/portfolioweb',
-//         assetPrefix: '/portfolioweb',
-//         eslint: {
-//             ignoreDuringBuilds: true,
-//         },
-//     }
-// }
