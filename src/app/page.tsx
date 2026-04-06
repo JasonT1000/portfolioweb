6bc2b86e3a3b1ee6fc199b244ea7f2c6be9c813b
JasonTollison <jason_tollison@hotmail.com>
2026-04-06 18:00:28 +1200

    Fixed button styles in light theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
       {activePageSection === PageSection.info && <Info />}
       {activePageSection === PageSection.contact && <Contact />}
 
-      <div className="hidden group absolute lg:h-auto hover:h-auto overflow-hidden bottom-10 mb-3 lg:mb-0 md:grid text-center lg:grid-cols-5 lg:text-left rounded-lg hover:bg-black/90 hover:lg:bg-inherit bg-black/100 transition ease-in-out duration-300">
+      <div className="hidden group absolute lg:h-auto hover:h-auto overflow-hidden bottom-10 mb-3 lg:mb-0 md:grid text-center lg:grid-cols-5 lg:text-left rounded-lg hover:bg-black/90 hover:lg:bg-inherit dark:bg-black/100 transition ease-in-out duration-300">
         <ButtonMain title={"Introduction"} description={"Just some info about this website"} updateActivePageSection={updateActivePageSection} activePageSection={activePageSection} />
         <ButtonMain title={"Work"} description={"The projects I have worked on over the last few years for employers."} updateActivePageSection={updateActivePageSection} activePageSection={activePageSection} />
         <ButtonMain title={"Personal projects"} description={"Some personal projects I have been working on in my spare time."} updateActivePageSection={updateActivePageSection} activePageSection={activePageSection} />
@@ -48,7 +48,7 @@ export default function Home() {
 
       {
         showMobileNav &&
-        <div className="md:hidden absolute hover:h-auto overflow-hidden bottom-10 mb-3 grid text-center rounded-lg hover:bg-black/90 bg-black/100 transition ease-in-out duration-300">
+        <div className="md:hidden absolute hover:h-auto overflow-hidden bottom-10 mb-3 grid text-center rounded-lg bg-neutral-200 hover:dark:bg-black/90 dark:bg-black/100 transition ease-in-out duration-300">
           <ButtonMain title={"Introduction"} description={"Just some info about this website"} updateActivePageSection={updateActivePageSection} activePageSection={activePageSection} />
           <ButtonMain title={"Work"} description={"The projects I have worked on over the last few years for employers."} updateActivePageSection={updateActivePageSection} activePageSection={activePageSection} />
           <ButtonMain title={"Personal projects"} description={"Some personal projects I have been working on in my spare time."} updateActivePageSection={updateActivePageSection} activePageSection={activePageSection} />
